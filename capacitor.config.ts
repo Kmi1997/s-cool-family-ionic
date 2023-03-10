18775a595030427121670a1fd5c9ec38bc032683
Camille <89855408+Kmi1997@users.noreply.github.com>
2023-03-10 11:02:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: "s'Cool Family",
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
